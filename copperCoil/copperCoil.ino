464336f6c9e4b1f11cb07f9b99bfb4e03057ebfa
HeyJOe03 <jocarmi03@gmail.com>
2021-10-27 12:58:16 +0300

    fix acceleration

--- copperCoil/copperCoil.ino
+++ copperCoil/copperCoil.ino
@@ -8,6 +8,8 @@ int ENASERVO = 3; //define Enable Pin
 int val = 0;
 int buttonState;
 int lastButtonState = LOW;
+const int delayServo = 9;
+const int delayStep = 50;
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 30;
 
@@ -34,7 +36,7 @@ void setup() {
       }
     }
   } {
-    for (int i = 0; i < 1530; i++) {
+    for (int i = 0; i < 1530; i++) { //starter position for the coil
       digitalWrite(DIRSTEP, LOW);
       digitalWrite(PULSTEP, HIGH);
       delay(1);
@@ -43,7 +45,7 @@ void setup() {
     }
 
   } {
-    for (int i = 0; i < 400; i++) {
+    for (int i = 0; i < 400; i++) { //400 servo revolution (bottom)
       digitalWrite(DIRSERVO, HIGH);
       digitalWrite(ENASERVO, HIGH);
       digitalWrite(PULSERVO, HIGH);
@@ -66,10 +68,82 @@ void loop()
       buttonState = reading;
       if (buttonState == HIGH) {
 
+
+
         {
-          for (int i = 0; i < 34; i++)
+          for (int layer = 0; layer < 34; layer++) //layers number = (x4)
 
           {
+            //RIGHT
+            {
+              for (int g = 436; g > 0; g--) { //layer length
+
+                if(layer == 0){ //SMOOOTH ACCELLERATION
+                  
+
+                  for (int i = 0; i < 400; i++) {
+                      digitalWrite(DIRSERVO, LOW);
+                      digitalWrite(ENASERVO, HIGH);
+                      digitalWrite(PULSERVO, HIGH);
+                      delayMicroseconds(delayServo * int(g / 32));
+                      digitalWrite(PULSERVO, LOW);
+                      delayMicroseconds(delayServo* int(g / 32));
+                    }
+
+                  for (int i = 0; i < 5; i++) { //0.026 mm
+                    digitalWrite(DIRSTEP, LOW); //DX
+                    digitalWrite(PULSTEP, HIGH);
+                    delayMicroseconds(delayStep* int(g / 32));
+                    digitalWrite(PULSTEP, LOW);
+                    delayMicroseconds(delayStep* int(g / 32));
+                  }
+                }
+
+                else{
+                    {
+                    for (int i = 0; i < 400; i++) {
+                      digitalWrite(DIRSERVO, LOW);
+                      digitalWrite(ENASERVO, HIGH);
+                      digitalWrite(PULSERVO, HIGH);
+                      delayMicroseconds(9);
+                      digitalWrite(PULSERVO, LOW);
+                      delayMicroseconds(9);
+                    }
+                  }
+                  for (int i = 0; i < 5; i++) { //0.026 mm
+                    digitalWrite(DIRSTEP, LOW); //DX
+                    digitalWrite(PULSTEP, HIGH);
+                    delayMicroseconds(50);
+                    digitalWrite(PULSTEP, LOW);
+                    delayMicroseconds(50);
+                  }
+
+                }
+              }
+            }
+            //LEFT
+            {
+              for (int i = 0; i < 436; i++) {
+                {
+                  for (int i = 0; i < 400; i++) {
+                    digitalWrite(DIRSERVO, LOW);
+                    digitalWrite(ENASERVO, HIGH);
+                    digitalWrite(PULSERVO, HIGH);
+                    delayMicroseconds(9);
+                    digitalWrite(PULSERVO, LOW);
+                    delayMicroseconds(9);
+                  }
+                }
+                for (int i = 0; i < 5; i++) {
+                  digitalWrite(DIRSTEP, HIGH); //SX
+                  digitalWrite(PULSTEP, HIGH);
+                  delayMicroseconds(50);
+                  digitalWrite(PULSTEP, LOW);
+                  delayMicroseconds(50);
+                }
+              }
+            }
+            //RIGHT
             {
               for (int i = 0; i < 436; i++) {
                 {
@@ -91,51 +165,7 @@ void loop()
                 }
               }
             }
-
-            {
-              for (int i = 0; i < 436; i++) {
-                {
-                  for (int i = 0; i < 400; i++) {
-                    digitalWrite(DIRSERVO, LOW);
-                    digitalWrite(ENASERVO, HIGH);
-                    digitalWrite(PULSERVO, HIGH);
-                    delayMicroseconds(9);
-                    digitalWrite(PULSERVO, LOW);
-                    delayMicroseconds(9);
-                  }
-                }
-                for (int i = 0; i < 5; i++) {
-                  digitalWrite(DIRSTEP, HIGH);
-                  digitalWrite(PULSTEP, HIGH);
-                  delayMicroseconds(50);
-                  digitalWrite(PULSTEP, LOW);
-                  delayMicroseconds(50);
-                }
-              }
-            }
-
-            {
-              for (int i = 0; i < 436; i++) {
-                {
-                  for (int i = 0; i < 400; i++) {
-                    digitalWrite(DIRSERVO, LOW);
-                    digitalWrite(ENASERVO, HIGH);
-                    digitalWrite(PULSERVO, HIGH);
-                    delayMicroseconds(9);
-                    digitalWrite(PULSERVO, LOW);
-                    delayMicroseconds(9);
-                  }
-                }
-                for (int i = 0; i < 5; i++) {
-                  digitalWrite(DIRSTEP, LOW);
-                  digitalWrite(PULSTEP, HIGH);
-                  delayMicroseconds(50);
-                  digitalWrite(PULSTEP, LOW);
-                  delayMicroseconds(50);
-                }
-              }
-            }
-
+            //LEFT + 1 TURN
             {
               for (int i = 0; i < 437; i++) {
                 {
