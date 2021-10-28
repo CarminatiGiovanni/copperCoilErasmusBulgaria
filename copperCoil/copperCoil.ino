92ed863ff23e6cabf51b904c4592c93f143dd96d
HeyJOe03 <jocarmi03@gmail.com>
2021-10-28 09:02:09 +0200

    changed left-right

--- copperCoil/copperCoil.ino
+++ copperCoil/copperCoil.ino
@@ -2,16 +2,17 @@
 #define BUTTON  8 //pin start button
 #define DIRSTEP 7 //pin for stepper for the Direction
 #define PULSTEP  6 //pin for the stepper pulse
-#define PULSERVO 5 //pin for servo pulse
+#define PULSERVO stepperMove //pin for servo pulse
 #define DIRSERVO  4 //pin for servo Direction
 //#define ENASERVO 3 //define Enable Pin delayServo
 
 const int delayServo = 9;
-const int delayStep = 50;
+const int delayStep = stepperMove0;
 const unsigned long debounceDelay = 30;
 
-const int coilStarterPosition = 1530;
+const int coilStarterPosition = 1stepperMove30;
 const servoRotation = 400;
+const stepperMove = 5;
 
 int val = 0;
 int buttonState;
@@ -70,95 +71,32 @@ void loop()
 
                 if(layer == 0){ //SMOOOTH ACCELLERATION
                   for (int i = 0; i < servoRotation; i++) pulseOutServo(LOW,delayServo*int(g / 8));//1 turn revolutionTime = delayServo * 2 * servoRotation * (g / 8)
-                  for (int i = 0; i < 5; i++) pulseOutStep(LOW, delayStep*int(g/8)) //0.026 mm
+                  for (int i = 0; i < stepperMove; i++) pulseOutStep(LOW, delayStep*int(g/8)) //0.026 mm
                 }
 
                 else{
-                    {
-                    for (int i = 0; i < servoRotation; i++) {
-                      digitalWrite(DIRSERVO, LOW);
-                      digitalWrite(ENASERVO, HIGH);
-                      digitalWrite(PULSERVO, HIGH);
-                      delayMicroseconds(9);
-                      digitalWrite(PULSERVO, LOW);
-                      delayMicroseconds(9);
-                    }
-                  }
-                  for (int i = 0; i < 5; i++) { //0.026 mm
-                    digitalWrite(DIRSTEP, LOW); //DX
-                    digitalWrite(PULSTEP, HIGH);
-                    delayMicroseconds(50);
-                    digitalWrite(PULSTEP, LOW);
-                    delayMicroseconds(50);
-                  }
-
+                  for (int i = 0; i < servoRotation; i++)pulseOutServo(LOW,delayServo);
+                  for (int i = 0; i < stepperMove; i++) pulseOutStep(LOW,delayStep);  //0.026mm
                 }
               }
             }
             //LEFT
             {
               for (int i = 0; i < 436; i++) {
-                {
-                  for (int i = 0; i < servoRotation; i++) {
-                    digitalWrite(DIRSERVO, LOW);
-                    digitalWrite(ENASERVO, HIGH);
-                    digitalWrite(PULSERVO, HIGH);
-                    delayMicroseconds(9);
-                    digitalWrite(PULSERVO, LOW);
-                    delayMicroseconds(9);
-                  }
-                }
-                for (int i = 0; i < 5; i++) {
-                  digitalWrite(DIRSTEP, HIGH); //SX
-                  digitalWrite(PULSTEP, HIGH);
-                  delayMicroseconds(50);
-                  digitalWrite(PULSTEP, LOW);
-                  delayMicroseconds(50);
-                }
+                for (int i = 0; i < servoRotation; i++)pulseOutServo(LOW,delayServo);
+                for (int i = 0; i < stepperMove; i++) pulseOutStep(HIGH,delayStep);  //0.026mm
               }
             }
             //RIGHT
             {
-              for (int i = 0; i < 436; i++) {
-                {
-                  for (int i = 0; i < servoRotation; i++) {
-                    digitalWrite(DIRSERVO, LOW);
-                    digitalWrite(ENASERVO, HIGH);
-                    digitalWrite(PULSERVO, HIGH);
-                    delayMicroseconds(9);
-                    digitalWrite(PULSERVO, LOW);
-                    delayMicroseconds(9);
-                  }
-                }
-                for (int i = 0; i < 5; i++) {
-                  digitalWrite(DIRSTEP, LOW);
-                  digitalWrite(PULSTEP, HIGH);
-                  delayMicroseconds(50);
-                  digitalWrite(PULSTEP, LOW);
-                  delayMicroseconds(50);
-                }
-              }
+              for (int i = 0; i < servoRotation; i++)pulseOutServo(LOW,delayServo);
+              for (int i = 0; i < stepperMove; i++) pulseOutStep(LOW,delayStep);  //0.026mm
             }
             //LEFT + 1 TURN
             {
               for (int i = 0; i < 437; i++) {
-                {
-                  for (int i = 0; i < servoRotation; i++) {
-                    digitalWrite(DIRSERVO, LOW);
-                    digitalWrite(ENASERVO, HIGH);
-                    digitalWrite(PULSERVO, HIGH);
-                    delayMicroseconds(9);
-                    digitalWrite(PULSERVO, LOW);
-                    delayMicroseconds(9);
-                  }
-                }
-                for (int i = 0; i < 5; i++) {
-                  digitalWrite(DIRSTEP, HIGH);
-                  digitalWrite(PULSTEP, HIGH);
-                  delayMicroseconds(50);
-                  digitalWrite(PULSTEP, LOW);
-                  delayMicroseconds(50);
-                }
+                for (int i = 0; i < servoRotation; i++)pulseOutServo(LOW,delayServo);
+                for (int i = 0; i < stepperMove; i++) pulseOutStep(HIGH,delayStep);  //0.026mm
               }
             }
           }
