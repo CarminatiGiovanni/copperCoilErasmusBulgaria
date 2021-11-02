d28fc70ff1e61e5263d6e06fb3a1899cca7608c6
HeyJOe03 <jocarmi03@gmail.com>
2021-11-02 07:32:14 +0100

    cleared code python

--- python/serialComunicator.py
+++ python/serialComunicator.py
@@ -1,4 +1,3 @@
-#Importing Libraries
 import serial
 import json
 
@@ -7,10 +6,8 @@ BAUDRATE = 115200
 arduinoConsts = {}
 arduino = serial.Serial(port=PORT,baudrate=BAUDRATE,timeout=.1)
 def write_read(x):
-    #print(x)
     arduino.write(bytes(x, 'utf-8'))
     data = arduino.readline().decode()
-    #print('doone')
     if x == 'consts':
         try:
             data = json.loads(data)
