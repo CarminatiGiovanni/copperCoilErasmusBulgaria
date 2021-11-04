6d576f8df7e99560ca1f9f789ced524d22eea74d
HeyJOe03 <jocarmi03@gmail.com>
2021-11-04 11:43:23 +0100

    bug fix

--- python/valueReaderArduino.py
+++ python/valueReaderArduino.py
@@ -13,37 +13,6 @@ root.title("Coil Machine Data")
 root.configure(background='white')
 root.geometry("+550+160")
 
-def write_read(request): # write on arduino a request and read the answer
-    arduino.write(bytes(request, 'utf-8')) # write on arduino the string request (encoding utf-8 -> b'request')
-    data = arduino.readline().decode() # read the answer of arduino, decode (b'response' -> utf-8) and save the string in data
-    return data
-
-def GetData(arduino: serial.Serial):
-
-    global arduinoConsts
-
-    while write_read('test') == '': # because when the serial communication starts the first messages are confused this just makes call until the response is not null
-        pass
-
-    arduinoConsts = write_read('consts') # ask for the consts
-
-    try:
-        print(arduinoConsts)
-        arduinoConsts = ast.literal_eval(arduinoConsts)
-        createLables()
-        createValues()
-    except:
-        err = Label(root,text="error in json load")
-        err.grid(row=0,column=0,padx=10,pady=10)
-    #print(arduinoConsts) # print on console the constst
-
-try:
-    arduino = serial.Serial(port=PORT,baudrate=BAUDRATE,timeout=.1) # connect arduino
-    GetData(arduino)
-except:
-    err = Label(root,text="error in connection to arduino")
-    err.grid(row=0,column=0,padx=10,pady=10)
-
 #Show data function
 def createValues():
     row = 0
@@ -59,6 +28,37 @@ def createLables ():
         l = Label(root,text=key)
         l.grid(row=row,column=0,padx=10,pady=10)
         row += 1
+
+def write_read(request): # write on arduino a request and read the answer
+    arduino.write(bytes(request, 'utf-8')) # write on arduino the string request (encoding utf-8 -> b'request')
+    data = arduino.readline().decode() # read the answer of arduino, decode (b'response' -> utf-8) and save the string in data
+    return data
+
+def GetData(arduino: serial.Serial):
+
+    global arduinoConsts
+
+    while write_read('test') == '': # because when the serial communication starts the first messages are confused this just makes call until the response is not null
+        pass
+
+    arduinoConsts = write_read('consts') # ask for the consts
+
+    try:
+        arduinoConsts = ast.literal_eval(arduinoConsts)
+        print(arduinoConsts)
+        createLables()
+        createValues()
+    except:
+        err = Label(root,text="error in json load")
+        err.grid(row=0,column=0,padx=10,pady=10)
+    #print(arduinoConsts) # print on console the constst
+
+try:
+    arduino = serial.Serial(port=PORT,baudrate=BAUDRATE,timeout=.1) # connect arduino
+    GetData(arduino)
+except:
+    err = Label(root,text="error in connection to arduino")
+    err.grid(row=0,column=0,padx=10,pady=10)
 
 def start():
     if arduinoConsts != None:
